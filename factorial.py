a2ae2851ba8271e842551de31c089f0a6ad93f9b
shubham <kartikbhuwad6@gmail.com>
2025-02-12 15:17:32 +0530

    cc

--- factorial.py
+++ factorial.py
@@ -2,10 +2,11 @@ def fac(n):
     if n == 0 or n == 1:
         return 1
     else:
-        return n * factorial(n - 1)
+        return n * fac(n - 1)
 
 # Example usage
 num = int(input("Enter a number: "))
-print(f"The fact of {num} is {factorial(num)}")
+print(f"The fact of {num} is {fac(num)}")
+print("hello")
 
 
